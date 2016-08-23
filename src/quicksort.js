b83d4bb5b167411ac574ab31cf49cf51ba4f72d0
Vanderci Curvelo Junior <vandercijr@yahoo.com.br>
2016-08-22 22:21:09 -0300

    Start quick sort algorithm

--- src/quicksort.js
+++ src/quicksort.js
@@ -1,5 +1,18 @@
 module.exports =  {
 	quickSort : function (arr) {
+		var length = arr.length()
+		, pivot = arr[length / 2]
+		, i = 0
+		, f = length-1;
+
+		while (i < j) {
+			while (i)
+
+		}
+
+		if (pivot ) {}
+
+
 		return [1,2,3,4,5,6,7,8,9];
 	},
 };
